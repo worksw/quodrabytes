d288cd354e616f561208b8bef698e62ea191866d
worksw <166789712+worksw@users.noreply.github.com>
2024-04-12 11:58:36 +0530

    Add files via upload

---
+++ app/assets/images/email.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const EmailIcon = () => {
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 31 31"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M27.9599 5.08374H2.54181V25.4182H27.9599V5.08374ZM25.4181 10.1674L15.2508 16.5219L5.08362 10.1674V7.62555L15.2508 13.9801L25.4181 7.62555V10.1674Z"
+        fill="#444444"
+      />
+    </svg>
+  );
+};
+
+export default EmailIcon;
---
+++ app/assets/images/navbarmenu.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Navbarmenu = () => {
+  return (
+    <svg height="20" width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M4 6H20M4 12H20M4 18H20"
+          stroke="white"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
+
+export default Navbarmenu;
